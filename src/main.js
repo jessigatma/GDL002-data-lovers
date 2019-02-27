d1ea62042138cf08ca107c5f76231bccacafa370
Your Name <you@example.com>
2019-02-27 08:05:27 -0600

    tomé el archivo array.js

--- src/main.js
+++ src/main.js
@@ -1,27 +1,12 @@
-function peruIndicators(){
+function showPeruIndicators(){        
 
- const economicIndicatorPeru = WORLDBANK.PER.indicators;
+    const dataPerIndicators = WORLDBANK.PER.indicators;
+    const select = document.getElementById("peruindicadores");
 
- 
-    //const choosedCountry = document.getElementById("listCountry").value; //toma el valor que el usuario escoge en la lista
-    //const userName = document.getElementById("policeName").value; // toma el nombre que da el usuario
-
-      //document.getElementById("rankWelcome").innerHTML = choosedCountry; //el valor dado al rango, lo arroja en el HTML
-      //document.getElementById("nameWelcome").innerHTML= userName; //el nombre del usuario lo da al HTML
-      
-    //const firstScreen = document.getElementById("firstScreen");
-    //const secondScreen = document.getElementById("secondScreen");
-    
-       //firstScreen.style.display = "none";
-       //secondScreen.style.display = "block"; //me muestra la segunda pantalla
+    for (let i= 0; i<= dataPerIndicators.length; i++){
+        let option = document.createElement("option"); //Creamos la opcion
+        option.innerHTML = dataPerIndicators[i].indicatorName; //Metemos el texto en la opción
+        select.appendChild(option); //Metemos la opción en el select
+    }
 }
-//document.getElementById("peru_button").addEventListener("click",continueClick,false); //Acciona el botón de Continuar
- 
-
-const newdataper = WORLDBANK.PER.indicators;
-
-for (let i= 0; i<= newdataper.length; i++){
-
-  console.log(WORLDBANK.PER.indicators[i].indicatorName);
-
-}
+showPeruIndicators();    
