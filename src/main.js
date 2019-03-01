930f15e8f2b965d7ea3d78920e7a619ef81993fd
Your Name <you@example.com>
2019-03-01 09:48:27 -0600

    se ordena el arreglo de mayor a menor en los datos por años

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 //SE EDUCATIVO
 
 function showPeruEconomicIndicators(){
-    // console.log("funcionperu");
+    
      const dataPerIndicators=WORLDBANK.PER.indicators;
      const economicIndicators = dataPerIndicators.filter(economicIndicator => economicIndicator.indicatorCode.includes("IC."));
      const select= document.getElementById("peruEconomicIndicators");
@@ -47,28 +47,36 @@ function showPeruEducationIndicators(){
 }
 showPeruEducationIndicators();
  
+
+ //---------------MUESTRA LOS AÑOS--------------------------
+
 function showAnios(){
 
     const dataPerIndicators = WORLDBANK.PER.indicators;
-    const  dataEconomicIndicator= dataPerIndicators.filter(indicator => indicator.indicatorCode.includes("IC."));
-    const  anioEconomicIndicator = Object.entries(dataEconomicIndicator[0].data); //el objeto lo convertimos en array 57 arreglos (1960 - 2017) de 2 entradas
+    const dataEconomicIndicator= dataPerIndicators.filter(indicator => indicator.indicatorCode.includes("IC."));
+    const anioEconomicIndicator = Object.entries(dataEconomicIndicator[0].data); //el objeto lo convertimos en array 57 arreglos (1960 - 2017) de 2 entradas
     const select = document.getElementById("anios"); //ve a los elemntos del select con id anios
  
     anioEconomicIndicator.forEach(function(element){
         let option = document.createElement("option"); //crea los elementos en la variable option
         option.innerHTML = element[0]; //innerHTML imprime los elementos en el HTML
-        select.appendChild(option); //ponlos en el select como hijos
+        select.appendChild(option); //ponlos en la variable select, que es un document.getElementbyId, como hijos
     });
 }
 showAnios();
 
 
-    
- 
+    const dataPerIndicators = WORLDBANK.PER.indicators;
+    const dataEconomicIndicator= dataPerIndicators.filter(indicator => indicator.indicatorCode.includes("IC."));
+    const yearEconomicIndicator = Object.entries(dataEconomicIndicator[2].data);//me regresa los data (año y porcentaje) del indicador en posicion 10 del tipo SL
 
 
+    const orderData = yearEconomicIndicator.sort(function(a,b){return a[1] - b[1]});//Me acomoda los array con los valores de la posición 1 de menor a mayor
+    const reverseOrderData=orderData.reverse(); //Me ordena de mayor a menor al arreglo anterior
+    console.log (reverseOrderData);
 
+  
 
-
-
+//const pegamos= WORLDBANK.BRA.indicators.map(({indicatorCode,indicatorName,}) => [`${indicatorCode}, ${indicatorName}`]);
+//console.log(pegamos);
 
