64b4ba3160b06e7372f0d5983f907a92a992bb01
Your Name <you@example.com>
2019-03-03 21:18:06 -0600

    acomodando las funciones en data.js

--- src/arrays.js
+++ src/arrays.js
@@ -1,27 +1,32 @@
-/*newWorldBank = [WORLDBANK.PER.indicators[2], WORLDBANK.PER.indicators[3],WORLDBANK.PER.indicators[11],
-WORLDBANK.PER.indicators[12],WORLDBANK.PER.indicators[14],WORLDBANK.PER.indicators[15],WORLDBANK.PER.indicators[17], 
-WORLDBANK.PER.indicators[18],WORLDBANK.PER.indicators[107],WORLDBANK.PER.indicators[109],WORLDBANK.PER.indicators[38],
-WORLDBANK.PER.indicators[39],WORLDBANK.PER.indicators[45],WORLDBANK.PER.indicators[46],WORLDBANK.PER.indicators[47],
-WORLDBANK.PER.indicators[50],WORLDBANK.PER.indicators[102],WORLDBANK.PER.indicators[103],WORLDBANK.PER.indicators[104],
-WORLDBANK.PER.indicators[120],WORLDBANK.PER.indicators[124],WORLDBANK.PER.indicators[125],WORLDBANK.PER.indicators[129],
-WORLDBANK.PER.indicators[134],WORLDBANK.PER.indicators[135], 
-WORLDBANK.MEX.indicators[8],WORLDBANK.MEX.indicators[9], WORLDBANK.MEX.indicators[15],WORLDBANK.MEX.indicators[16],WORLDBANK.MEX.indicators[17],
-WORLDBANK.MEX.indicators[77],WORLDBANK.MEX.indicators[79],WORLDBANK.MEX.indicators[95],WORLDBANK.MEX.indicators[101],WORLDBANK.MEX.indicators[102],
-WORLDBANK.MEX.indicators[103],WORLDBANK.MEX.indicators[104],WORLDBANK.MEX.indicators[105],WORLDBANK.MEX.indicators[106],WORLDBANK.MEX.indicators[107],
-WORLDBANK.MEX.indicators[108],WORLDBANK.MEX.indicators[110],WORLDBANK.MEX.indicators[111],WORLDBANK.MEX.indicators[113],WORLDBANK.MEX.indicators[114],
-WORLDBANK.MEX.indicators[116],WORLDBANK.MEX.indicators[117],WORLDBANK.MEX.indicators[129],WORLDBANK.MEX.indicators[134],WORLDBANK.MEX.indicators[135], 
-WORLDBANK.BRA.indicators[0],WORLDBANK.BRA.indicators[6],WORLDBANK.BRA.indicators[7],WORLDBANK.BRA.indicators[8],WORLDBANK.BRA.indicators[19],
-WORLDBANK.BRA.indicators[20],WORLDBANK.BRA.indicators[72],WORLDBANK.BRA.indicators[73],WORLDBANK.BRA.indicators[74],WORLDBANK.BRA.indicators[77],
-WORLDBANK.BRA.indicators[77],WORLDBANK.BRA.indicators[90],WORLDBANK.BRA.indicators[94],WORLDBANK.BRA.indicators[95],WORLDBANK.BRA.indicators[101],
-WORLDBANK.BRA.indicators[102],WORLDBANK.BRA.indicators[110],WORLDBANK.BRA.indicators[111],WORLDBANK.BRA.indicators[113],WORLDBANK.BRA.indicators[114],
-WORLDBANK.BRA.indicators[116],WORLDBANK.BRA.indicators[117],WORLDBANK.BRA.indicators[129],WORLDBANK.BRA.indicators[134],WORLDBANK.BRA.indicators[135],
-WORLDBANK.CHL.indicators[2],WORLDBANK.CHL.indicators[3],WORLDBANK.CHL.indicators[11],WORLDBANK.CHL.indicators[12],WORLDBANK.CHL.indicators[14],
-WORLDBANK.CHL.indicators[15],WORLDBANK.CHL.indicators[17],WORLDBANK.CHL.indicators[18],WORLDBANK.CHL.indicators[38],WORLDBANK.CHL.indicators[39],
-WORLDBANK.CHL.indicators[45],WORLDBANK.CHL.indicators[46],WORLDBANK.CHL.indicators[47],WORLDBANK.CHL.indicators[50],WORLDBANK.CHL.indicators[102],
-WORLDBANK.CHL.indicators[103],WORLDBANK.CHL.indicators[104],WORLDBANK.CHL.indicators[106],WORLDBANK.CHL.indicators[107],WORLDBANK.CHL.indicators[109],
-WORLDBANK.CHL.indicators[120],WORLDBANK.CHL.indicators[125],WORLDBANK.CHL.indicators[129],WORLDBANK.CHL.indicators[134],WORLDBANK.CHL.indicators[135]];
-*/
-//console.log(WORLDBANK);
+//const pegamos1= WORLDBANK.MEX.indicators.map(({indicatorCode,}) => [` ${indicatorCode}`]);
+//console.log(pegamos1);
+
+//const data1 = WORLDBANK.PER.indicators;
+//const  data2= data1.filter(indicator => indicator.indicatorCode.includes("IC."));
+//console.log(data2[0].indicatorName);
+
+// -------- del tipo de indicador que se escoge, vemos los datos: año-%
+
+//const  aniosindicador=Object.entries(data2[0].data); //el objeto lo convertimos en array 57 arreglos (1960 - 2017) de 2 entradas
+// console.log(aniosindicador);
+
+//aniosindicador.forEach(function(anios){ //me imprime todos los años
+  //console.log(`año: ${anios[0]}   porcentaje: ${anios[1]}`)
+//})
+
+
+
+
+
+//console.log(datos2[0].data[2010]);
+
+/*let aniosindicador = Object.entries(datos1[0].data);
+//console.log(aniosindicador);
+
+aniosindicador.forEach(function(element){
+    //console.log("año:"+ element[0] + " valor: "  + element[1]);
+    console.log(`year: ${element[0]} valor: ${element[1]}`);
+});*/
 
 //const pegamos= WORLDBANK.BRA.indicators.map(({indicatorCode,indicatorName,}) => [`${indicatorCode}, ${indicatorName}`]);
 //console.log(pegamos);
--- src/data.js
+++ src/data.js
@@ -1,38 +1,25 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-/*const example = () => {
+/*
+window.dataLovers= {
+
+  filtering: function (,){
+
+  },
+
+  ordering: function (,){
+
+  },
+
+  calculating: function (,){
+    
+  }
+
+};
+------------------
+const example = () => {
   return 'example';
 };
 
 window.example = example; */
-
-//const pegamos1= WORLDBANK.MEX.indicators.map(({indicatorCode,}) => [` ${indicatorCode}`]);
-//console.log(pegamos1);
-
-const data1 = WORLDBANK.PER.indicators;
-const  data2= data1.filter(indicator => indicator.indicatorCode.includes("IC."));
-//console.log(data2[0].indicatorName);
-
-// -------- del tipo de indicador que se escoge, vemos los datos: año-%
-
-const  aniosindicador=Object.entries(data2[0].data); //el objeto lo convertimos en array 57 arreglos (1960 - 2017) de 2 entradas
-// console.log(aniosindicador);
-
-aniosindicador.forEach(function(anios){ //me imprime todos los años
-  //console.log(`año: ${anios[0]}   porcentaje: ${anios[1]}`)
-})
-
-
-
-
-
-//console.log(datos2[0].data[2010]);
-
-/*let aniosindicador = Object.entries(datos1[0].data);
-//console.log(aniosindicador);
-
-aniosindicador.forEach(function(element){
-    //console.log("año:"+ element[0] + " valor: "  + element[1]);
-    console.log(`year: ${element[0]} valor: ${element[1]}`);
-});*/
